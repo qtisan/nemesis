f411037d8f6430dccc53b0eb5968941fef6fe15c
Saul QIAN <lennon@imqx.com>
2019-02-15 13:22:08 +0800

    fixed the bug from log4js

--- examples/config/log4js.js
+++ examples/config/log4js.js
@@ -3,7 +3,7 @@ const log4js = require('log4js');
 
 log4js.configure({
   appenders: {
-    SMOEC: { type: 'file', filename: 'example/logs/smoec.log' }
+    SMOEC: { type: 'file', filename: 'examples/logs/smoec.log' }
   },
   categories: {
     default: { appenders: ['SMOEC'], level: 'info' }
